a5742012e85389a9e54e1e1bb67176b5afadbd2f
Ziel <frans_421_12@hotmail.com>
2022-07-15 00:37:34 -0500

    feat: carrito count vuex

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,22 @@ require('./bootstrap');
 
 window.Vue = require('vue').default;
 
+const Vuex = require('vuex').default;
+
+window.store = new Vuex.Store({
+    state: {
+        productsCount: 2
+    }, 
+    mutations: {
+        increment(state){
+            return state.productsCount++
+        },
+        set(state, value){
+            return state.productsCount = value
+        }
+    }
+})
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -23,6 +39,7 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 Vue.component('products-component', require('./components/products/ProductsComponent.vue').default);
 Vue.component('add-product-btn', require('./components/products/AddToCartComponent.vue').default);
+Vue.component('product-counter-component', require('./components/shopping_cart/CounterComponent.vue').default);
 Vue.component('product-card-component', require('./components/products/ProductCardComponent.vue').default);
 Vue.component('material-transition-group', require('./components/animations/MaterialCollectionComponent.vue').default);
 
---
+++ resources/js/components/shopping_cart/CounterComponent.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+    <div>
+        <a href="/carrito">Mi carrito {{ productsCount }}</a>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ["count"],
+        created(){
+            window.store.commit("set", this.count);
+        },
+        computed: {
+            productsCount(){
+                return window.store.state.productsCount;
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
